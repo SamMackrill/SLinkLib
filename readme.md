e2ea51a2e83adb0773897b2db5ce1ca08455aa78
Sam Mackrill <sam.mackrill@aveva.com>
2019-01-21 10:48:40 +0000

    Case ==> lower

---
+++ readme.md
@@ -0,0 +1,2 @@
+# SLinkLib
+SourceLink testing library
---
+++ slinklib/slinklib.h
@@ -0,0 +1,11 @@
+#pragma once
+
+using namespace System;
+
+namespace SLinkLib {
+	public ref class Class1
+	{
+	public:
+		static int fn() { return 99; }
+	};
+}
